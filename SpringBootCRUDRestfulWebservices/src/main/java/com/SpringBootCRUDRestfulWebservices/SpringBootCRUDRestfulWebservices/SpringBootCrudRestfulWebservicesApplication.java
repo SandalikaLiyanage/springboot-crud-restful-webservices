e37e5e4e393e439f43c07b5a035db8e362e5e188
SandalikaLiyanage <sandalika24@gmail.com>
2024-07-08 13:32:37 +0530

    first commit

---
+++ SpringBootCRUDRestfulWebservices/src/main/java/com/SpringBootCRUDRestfulWebservices/SpringBootCRUDRestfulWebservices/SpringBootCrudRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootCRUDRestfulWebservices.SpringBootCRUDRestfulWebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudRestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ SpringBootCRUDRestfulWebservices/src/test/java/com/SpringBootCRUDRestfulWebservices/SpringBootCRUDRestfulWebservices/SpringBootCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootCRUDRestfulWebservices.SpringBootCRUDRestfulWebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
